362b95a7e8295236a3fc54f9825d86e2930d22c8
olakpa <olakpa@sheridancollege.ca>
2023-09-23 16:23:39 -0400

    This is my second Script

--- TEST.ps1
+++ TEST.ps1
@@ -2,3 +2,8 @@ $date = get-date
 $name = $env:USERNAME
 Write-host "Hello world!!, Your name is $name, Today is $date"
 git add “TEST.ps1”
+
+$date = get-date
+$name = $env:USERNAME
+Write-host "Hello world!!, Your name is $name, Today is $date"
+git add “TEST.ps1”
